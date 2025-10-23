2ce1864c1843ce7eeb395b06b58defa46322cd97
labomb712 <ginong00@gmail.com>
2025-10-23 17:21:44 +0900

    채팅방 방제 업데이트

--- rookies-oneteam/src/main/java/com/store/rookiesoneteam/service/impl/UserServiceImpl.java
+++ rookies-oneteam/src/main/java/com/store/rookiesoneteam/service/impl/UserServiceImpl.java
@@ -23,7 +23,7 @@ import java.util.regex.Pattern;
 @Service("userServiceImpl")
 @RequiredArgsConstructor
 @Slf4j
-public class UserServiceImpl implements UserService {
+public class    UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
